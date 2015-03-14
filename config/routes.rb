da292d3bdf4add1ea2dad7ee9b01c8997fca2791
Kenny T <ktrionfo@integracore.com>
2015-03-14 16:10:29 -0600

    changed root to restaruants

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :restaurants
 
   devise_for :users
-  root to: "home#index"
+  root to: "restaurants#index"
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314213859) do
+ActiveRecord::Schema.define(version: 20150314215138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "address"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
